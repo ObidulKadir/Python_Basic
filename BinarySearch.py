e69c578428efdaa841e7e8da80dbe4c1757aaa3f
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2020-08-30 16:39:14 +0000

    Simple Way of Binary Search algorithm

---
+++ BinarySearch.py
@@ -0,0 +1,26 @@
+#Binary search 
+pos = -1
+def Binary_Search(list,key):
+	l = 0  #lower bound index
+	u = len(list) - 1 #upper bound index
+
+	while l <= u :
+		mid = (l+u) // 2
+
+		if list[mid] == key:
+			globals()['pos'] = mid
+			return True
+		else:
+			if list[mid] < key :
+				l = mid + 1
+			else:
+				l = mid - 1
+	else:
+		return False
+
+list = [3,4,5,6,9,10]
+key  = 10
+if Binary_Search(list,key):
+	print("Item is found at ",pos+1)
+else:
+	print("Not found")
