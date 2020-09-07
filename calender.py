7e59be08372ab379e2a33bf32504dcb619a0c92f
S.K.Obidul kadir <rezwankadirobaidul@gmail.com>
2020-09-07 04:48:32 +0000

    Calendar Module.

---
+++ calender.py
@@ -0,0 +1,19 @@
+import calendar
+''''
+print(calendar.month(2020,9))
+print(calendar.firstweekday())
+print()
+print(calendar.weekheader(7))
+'''
+print(calendar.monthrange(2020,9))
+
+print()
+print(calendar.month(2020,9,w = 4, l = 3))
+print()
+print(calendar.calendar(2020))
+print()
+print(calendar.monthcalendar(2020,9))
+print(calendar.isleap(2020))
+
+print()
+print(calendar.leapdays(0,2021))
